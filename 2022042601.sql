026052c1c8a3bb42d4e561b59381509bb49b1fb5
Ms-97 <58967787+Ms-97@users.noreply.github.com>
2022-04-28 18:16:43 +0900

    0428

--- 2022042601.sql
+++ 2022042601.sql
@@ -129,6 +129,41 @@ INTERSECT
      AND B.PARENT_ID= A.DEPARTMENT_ID
      AND C.PARENT_ID= B.DEPARTMENT_ID
      ORDER BY 5;
+----------------------------------------------------------------------------     
+**계층형 쿼리 --------------------    220428(추가)
+ - 계층적 구조를 지닌 테이블의 내용을 출력할때 사용
+ - 트리구조를 이용한 방식
+  (사용형식)
+   SELECT 컬럼list 
+     FROM 테이블명
+    START WITH 조건 -- 루트(root)노드 지정
+    CONNECT BY NOCYCLE|PRIOR 계층구조 조건 --계층구조가 어떤식으로 
+                                          --연걸 되었는지 설정
+** CONNECT BY PRIOR 자식컬럼 - 부모컬럼 : 부모에서 자식으로 
+                                        트리구성(TOP DOWN) 
+   CONNECT BY PRIOR 부모컬럼 - 자식컬럼 : 자식에서 부모로 
+                                        트리구성(BOTTOM UP)
+
+** PRIOR 사용위치에 따른 방향 
+   CONNECT BY PRIOR 컬럼1 - 컬럼2
+                    <-----------
+   CONNECT BY 컬럼1 PRIOR - 컬럼2
+                    ----------->
+                    
+**계층형 쿼리 확장 --------------------    220428(추가)
+    CONNECT_BY_ROOT 컬럼명 :  루트노드 찾기
+    CONNECT_BY_ISCYCLE : 중복참조값 찾기
+    CONNECT_BY_ISLEAF : 단말노드 찾기
+    
+SELECT DEPARTMENT_ID AS 부서코드,
+       LPAD(' ',4*(LEVEL-1))||DEPARTMENT_NAME AS 부서명,
+       LEVEL AS 레벨
+  FROM HR.DEPTS
+ START WITH PARENT_ID IS NULL
+ CONNECT BY PRIOR DEPARTMENT_ID=PARENT_ID
+ -- CONNECT BY PRIOR PARENT_ID = DEPARTMENT_ID;
+ ORDER SIBLINGS BY DEPARTMENT_NAME;
+--------------------------------------------------------------------------    
    
 사용예) 장바구니테이블에서 4월과 6월에 판매된 모든 상품정보를 중복되지 않게 조회하시오
        Alias는 상품번호,상품명,판매수량 이며 상품번호 순으로 출력하시오.
